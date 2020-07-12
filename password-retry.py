75af49208625de9d5af6ad7f87875546069b9b41
BOYOU LIN <boyoulin@BOYOUde-MacBook-Air.local>
2020-07-12 12:34:20 +0800

    add code

--- password-retry.py
+++ password-retry.py
@@ -1,10 +1,14 @@
 pwssword = 'a123456'
-i = 3
+i = 3 # 預設機會次數
 while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼： ')
 	if pwd == pwssword:
 		print('登錄成功')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤！ 還有',i , '次機會') 
+		print('密碼錯誤！ ') 
+		if i > 0:
+			print('還有',i , '次機會')
+		else:
+			print('帳號已鎖定')
